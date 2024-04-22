b2cd8d82d63e8577c8996d148a753519c738e5fc
MachunuruSreenivasulaRedd <918296629y@gmail.com>
2024-04-22 18:11:51 +0530

    changesadded

---
+++ client/src/components/index.js
@@ -0,0 +1,5 @@
+// eslint-disable-next-line import/no-cycle
+export { default as Welcome } from "./Welcome";
+export { default as Footer } from "./Footer";
+
+export { default as Transactions } from "./Transactions";
